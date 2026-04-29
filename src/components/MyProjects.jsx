f708d6c9c5d39bcd2c938d63ee0f7ade6a1f5c65
delepover56 <taha82426980@gmail.com>
2026-04-29 20:10:40 +0500

    6.1

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -9,13 +9,13 @@ const MyProjects = [
         name: "Tasbeeh Wala",
         link: "https://tasbeeh-wala.vercel.app/",
         image: "/Images/Projects/Tasbeeh_Wala.webp",
-        alt "Website screenshot of Tasbeeh Wala project"
+        alt: "Website screenshot of Tasbeeh Wala project",
     },
     {
         name: "True Vegan Sea Food",
         link: "https://true-vegan-seafood.vercel.app/",
         image: "/Images/Projects/True_Vegan_Sea_Food.webp",
-        alt "Website screenshot of True Vegan Sea Food project"
+        alt: "Website screenshot of True Vegan Sea Food project",
     }
 ];
 
